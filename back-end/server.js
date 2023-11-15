fd94370d7a05cbf0206fd1442cbafda3d459b751
Kumar Shresth <kumarshresth2004@gmail.com>
2023-11-15 14:05:37 +0530

    #

---
+++ back-end/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+mongoose.connect('mongodb://localhost:27017/Login')
+const UserSchema = new mongoose.Schema({
+    enrollmentNo:String,
+    password:String
+
+})
+
+const UserModel = mongoose.model("users",UserSchema);
+
+app.post("/login",(req,res)=>{
+    const {enrollmentNo,password} = req.body;
+    UserModel.findOne({enrollmentNo:enrollmentNo})
+    .then(user=>{
+        if(user){
+            if(user.password === password){
+               res.json("Login successfull") 
+            }
+
+            else{
+
+                res.json("The password is incorrect");
+
+            }
+        }
+
+        else{
+            res.json("No record existed");
+        }
+    })
+})
+
+app.listen(3001,()=>{
+    console.log('Server is running');
+})
