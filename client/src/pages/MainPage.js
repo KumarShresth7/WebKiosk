56a307eac1290521badb2ca94d73f9f3e51d5058
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-11-21 00:51:59 +0530

    Structure of the mainpage

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -3,98 +3,38 @@ import { Link } from 'react-router-dom';
 
 
 function MainPage() {
-  
+
   return (
-    <div>
-      <h1 className='bg-yellow-600'>
-        <div className='w-100'><img className='object-contain h-20 w-20 px-0 py-0' src='https://hindubabynames.info/wp-content/themes/hbn_download/download/education-companies/thapar-institute-of-engineering-and-technology-patiala-logo.png' alt=''/></div>
-        <div className='text-black text-center text-4xl'>THAPAR INSTITUTE OF ENGINEERING & TECHNOLOGY,PATIALA</div> 
-        <div className='text-right text-blue-600 text-xl pt-0'>Good <br/> Morning <br/> Kumar <br/> <Link className='underline text-white' to='/'>Signout</Link></div>
-      </h1>
-
-
-      <div className='mb-10 text-green-700'>
-      Available options
-      <br/>
-<label for="personal info">Personal Info</label>
-
-<select name="personal info" id="personal info">
-  <option value="volvo">Personal Details</option>
-  <option value="saab">Edit Info</option>
-  <option value="mercedes">Bank Info</option>
-  <option value="audi">Upload Docs</option>
-</select>
-
-<br/>
-
-<label for="personal info">SRS</label>
-
-<select name="personal info" id="personal info">
-  <option value="volvo">New SRS Entry</option>
-  <option value="saab">View Sent SRS</option>
-  
-</select>
-
-<br/>
-
-<label for="personal info">Grievances</label>
-
-<select name="personal info" id="personal info">
-  <option value="volvo">New Grievances Entry</option>
-  <option value="saab">View Submitted Grievances</option>
-  
-</select>
-
-<br/>
-
-<label for="personal info">Fee Detail</label>
-
-<select name="personal info" id="personal info">
-  <option value="volvo">Personal Details</option>
-  <option value="saab">Edit Info</option>
-  <option value="mercedes">Bank Info</option>
-  <option value="audi">Upload Docs</option>
-</select>
-
-<br/>
-
-<label for="personal info">Academic Info</label>
-
-<select name="personal info" id="personal info">
-  <option value="volvo">Personal Details</option>
-  <option value="saab">Edit Info</option>
-  <option value="mercedes">Bank Info</option>
-  <option value="audi">Upload Docs</option>
-</select>
-
-<br/>
-
-<label for="personal info">Exam Info</label>
-
-<select name="personal info" id="personal info">
-  <option value="volvo">Personal Details</option>
-  <option value="saab">Edit Info</option>
-  <option value="mercedes">Bank Info</option>
-  <option value="audi">Upload Docs</option>
-</select>
-
-    </div>
-
-    <div className='underline text-blue-400'>
-      <ol>
-      
-        <li><a href=''>UG-1st Yr-Grp A TIMETABLE</a></li>
-        <li><a href=''>UG-1st Yr-Grp B TIMETABLE</a></li>
-        <li><a href=''>UG-2nd Yr-Grp A TIMETABLE</a></li>
-        <li><a href=''>UG-2nd Yr-Grp B TIMETABLE</a></li>
-        <li><a href=''>UG-3rd Yr-Grp A TIMETABLE</a></li>
-        <li><a href=''>UG-3rd Yr-Grp B TIMETABLE</a></li>
-        <li><a href=''>UG-4th yr-Grp A TIMETABLE</a></li>
-        <Link to='/changePin'>Change Pin </Link> <br/>
-        <Link to='/'>Logout</Link>
-      </ol>
-    </div>
-      
+    <div className='bg-myColor h-screen'>
+      <nav className="bg-orange-400 px-4 flex justify-between py-3">
+        <div className="inline-block mb-3">
+          <p className="text-5xl font-serif">
+            Thapar University, Patiala
+          </p>
+        </div>
+        <div>
+          <p className="font-semibold">Welcome {"XYZ"}</p>
+          <p className="rounded-md text-center bg-red-500 p-1"><Link to={"/"}>Sign Out</Link></p>
+        </div>
+      </nav>
+      <div className="flex">
+        <div className=" bg-yellow-400 w-1/5 p-4">
+          <h1 className="text-2xl font-bold">Sidebar</h1>
+          <ul>
+            <li className="py-2"><Link href="#">Link 1</Link></li>
+            <li className="py-2"><Link href="#">Link 2</Link></li>
+            <li className="py-2"><Link href="#">Link 3</Link></li>
+          </ul>
+        </div>
+        <div className="w-=3/5">
+        <h1 className="text-2xl font-bold">Sidebar</h1>
+          <ul>
+            <li className="py-2"><Link href="#">Link 1</Link></li>
+            <li className="py-2"><Link href="#">Link 2</Link></li>
+            <li className="py-2"><Link href="#">Link 3</Link></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
